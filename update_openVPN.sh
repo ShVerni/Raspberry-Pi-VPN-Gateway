2f018b6c080bba46505d72d7be86c5e88959f4d0
Sam Groveman <shverni@gmail.com>
2017-08-09 18:55:14 -0400

    Added OpenVPN update script.

---
+++ update_openVPN.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Check for root access.
+if [ $(id -u) != "0" ]; then
+	echo "You must be the superuser to run this script."
+	exit 1
+fi
+NEW_VERSION=$(wget -qO - http://build.openvpn.net/downloads/releases/latest/LATEST.txt | head -1 | egrep -o '([0-9]\.?[0-9]\.?[0-9]\.?[0-9]?)')
+OLD_VERSION=$(openvpn --version | head -1 | egrep -o '([0-9]\.?[0-9]\.?[0-9]\.?[0-9]?) ' | tr -d '[:space:]')
+if [ "$NEW_VERSION" != "$OLD_VERSION" ]; then
+	wget http://build.openvpn.net/downloads/releases/latest/openvpn-latest-stable.tar.gz
+	systemctl stop monit.service
+	systemctl stop openvpn
+	mkdir openvpn-new
+	gzip -dc openvpn-latest-stable.tar.gz | tar -xf - -C openvpn-new --strip-components=1
+	cd openvpn-new/
+	./configure --prefix=/usr
+	make
+	make install
+	cd ..
+	rm openvpn-latest-stable.tar.gz
+	rm -R openvpn-new
+	systemctl start openvpn
+	systemctl start monit.service
+	echo "$(date +"%F %T") OpenVPN Updated" >> /home/pi/vpnfix.log
+fi
+exit 0
